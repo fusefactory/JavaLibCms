009f3360d03bbabef724259aa4f60c8d11b7bd6d
shortnotion (mark) <shortnotion@gmail.com>
2017-07-06 17:05:07 +0200

    some state class logic

--- src/main/java/com/fuse/cms/State.java
+++ src/main/java/com/fuse/cms/State.java
@@ -1,10 +1,27 @@
 package com.fuse.cms;
 
-
-public class State {
+/**
+ * The State class is an extension to the Model class
+ * designed for managing application state. It lets
+ * you create a hierarchical state structure; every
+ * State instance can access child- or parent-states,
+ * and all these states have access to the central state
+ * repository (which is simple a ModelCollection, as all
+ * states are in the end models).
+ */
+public class State extends Model {
     private ModelCollection collection;
 
+    /** Default constructor; creates it own collection
+     * collection as central state-repository and should therefore
+     * only be used to initialize an application's root state
+     * (though the collection can be overwritten by a later call
+     * to the setCollection method).
+     */
     public State(){
+      ModelCollection col = new ModelCollection();
+      col.add(this);
+      setCollection(col);
     }
 
     public State(ModelCollection collection){
@@ -17,5 +34,64 @@ public class State {
 
     public ModelCollection getCollection(){
          return collection;
+    }
+
+    public State child(String name){
+      return this.findById(childId(name), true);
+    }
+
+    public State parent(){
+      String _id = parentId();
+      return findById(_id, false);
+    }
+
+    public State childQuery(String query){
+      return findById(childId(query), true);
+    }
+
+    private State findById(String id, boolean createIfNotExist){
+      // find existing
+      Model m = collection.findById(id);
+      if(m != null)
+        return (State) m;
+
+      if(!createIfNotExist)
+        return null;
+
+      State newState = new State(collection);
+      newState.set("id", id);
+      collection.add(newState);
+      return newState;
+    }
+
+    /**
+     * Returns the id of the parent node
+     *
+     * @return String null if this is already the root state
+     */
+    private String parentId(){
+      String _id = getId();
+
+      if(_id.equals(""))
+        return null;
+
+      String[] parts = _id.split("\\.");
+
+      if(parts.length < 1)
+        return null;
+
+      if(parts.length == 1)
+        return "";
+
+      String parentId = "";
+      for(int i=0; i<parts.length-2; i++){
+        parentId = parentId + parts[i] + ".";
+      }
+      parentId = parentId + parts[parts.length-2];
+      return parentId;
+    }
+
+    private String childId(String childName){
+      return getId().equals("") ? childName : getId()+"."+childName;
     }
 }
--- src/test/java/com/fuse/cms/StateTest.java
+++ src/test/java/com/fuse/cms/StateTest.java
@@ -5,9 +5,36 @@ import org.junit.Test;
 
 public class StateTest {
   @Test
+  public void defaultCollection(){
+    assertEquals((new State()).getCollection() == null, false);
+  }
+
+  @Test
   public void getCollection(){
     ModelCollection col = new ModelCollection();
     State state = new State(col);
     assertEquals(state.getCollection(), col);
+  }
+
+  @Test
+  public void child_parent(){
+    State root = new State();
+    // create child node
+    State c1 = root.child("child1");
+    assertEquals(c1.getId(), "child1");
+    // fetch existing child node
+    assertEquals(root.child("child1"), c1);
+    // linking
+    assertEquals(c1.child("child2").child("child3").getId(), "child1.child2.child3");
+    assertEquals(c1.child("child2").child("child3").parent(), c1.child("child2"));
+    // parent
+    assertEquals(c1.parent(), root);
+    assertEquals(c1.parent().getId(), "");
+  }
+
+  @Test
+  public void childQuery(){
+    State root = new State();
+    assertEquals(root.childQuery("child1.child2.attribute"), root.child("child1").childQuery("child2.attribute"));
   }
 }
